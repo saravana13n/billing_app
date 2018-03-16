e4a5b0775e15b18e009bfaa3ee13a264bcdad6f6
saravana13n <saravana13n@gmail.com>
2018-03-16 19:24:18 +0530

    Update main.48f482ca423b26f6f383.bundle.js

--- main.48f482ca423b26f6f383.bundle.js
+++ main.48f482ca423b26f6f383.bundle.js
@@ -1,3 +1,9 @@
+
+document.addEventListener('deviceready', function(){
+  pdf.fromURL('http://cesarvr.github.io/2015/11/20/javascript-v8.html', opts)
+  .then((status) => console.log('success->', status))
+  .catch((error) => console.log(error));
+});
 webpackJsonp([1], {
     0: function(t, e, n) {
         t.exports = n("cDNt")
@@ -10799,15 +10805,9 @@ webpackJsonp([1], {
                     this.router.navigate(["/form"])
                 }, t.prototype.print = function() {
                     alert();
-                    document.addEventListener('deviceready', function(){
-                      pdf.fromURL('http://cesarvr.github.io/2015/11/20/javascript-v8.html', opts)
-                      .then((status) => console.log('success->', status))
-                      .catch((error) => console.log(error));
-                    })
-                    var t, e;
-                    t = document.getElementById("print-section").innerHTML, 
-                        
                     
+                    var t, e;
+                    t = document.getElementById("print-section").innerHTML,   
 
                 }, t.prototype.resetItems = function() {
                     this.data.storage = void 0
